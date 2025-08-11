3ca809d8080a2ea0176f78623b2c1bc84fe78ed3
dpvirk3 <dpvirk3@gmail.com>
2025-08-11 19:37:24 +0530

    Use Tika - wrapper on apache - to read files to load into the vector store.

--- spring-ai-rag-ollama/spring-ai-rag-ollama/src/main/java/com/dv/spring_ai_rag_ollama/config/VectorStoreConfig.java
+++ spring-ai-rag-ollama/spring-ai-rag-ollama/src/main/java/com/dv/spring_ai_rag_ollama/config/VectorStoreConfig.java
@@ -1,23 +1,55 @@
 package com.dv.spring_ai_rag_ollama.config;
 
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.ai.transformer.splitter.TextSplitter;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 
 import java.io.File;
+import java.util.List;
 
+@Slf4j
 @Configuration
 public class VectorStoreConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(VectorStoreConfig.class);
 
     @Bean
     public SimpleVectorStore simpleVectoreStore(EmbeddingModel embeddingModel,
                                                  VectorStoreProperties vectorStoreProperties) {
-        SimpleVectorStore simpleVectorStore = SimpleVectorStore.builder(embeddingModel).build();
+        var vectorStore = SimpleVectorStore.builder(embeddingModel).build();
 
         File vectorStorefile = new File(vectorStoreProperties.getVectorStorePath());
 
-        return simpleVectorStore;
+        if (vectorStorefile.exists()) {
+            vectorStore.load(vectorStorefile);
+        }
+        else {
+            log.debug("Loading documents into vector store");
+            vectorStoreProperties.getDocumentsToLoad().forEach(document -> {
+               System.out.println("Loading documents: " + document.getFilename());
+
+                TikaDocumentReader documentReader = new TikaDocumentReader(document);
+                //this gets the metadata about documents
+                List<Document> docs = documentReader.get();
+                TextSplitter textSplitter = new TokenTextSplitter();
+                //document is split into pieces before loading into vector store
+                List<Document> splitDocs = textSplitter.apply(docs);
+                //this add functions embeds the documents and then stored
+                vectorStore.add(splitDocs);
+            });
+
+            vectorStore.save(vectorStorefile);
+        }
+
+        return vectorStore;
     }
 }
--- spring-ai-rag-ollama/spring-ai-rag-ollama/src/main/java/com/dv/spring_ai_rag_ollama/config/VectorStoreProperties.java
+++ spring-ai-rag-ollama/spring-ai-rag-ollama/src/main/java/com/dv/spring_ai_rag_ollama/config/VectorStoreProperties.java
@@ -3,12 +3,22 @@ package com.dv.spring_ai_rag_ollama.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.util.List;
 
 @Configuration
 @ConfigurationProperties(prefix = "com.dv.aiapp")
 public class VectorStoreProperties {
 
     private String vectorStorePath;
+
+    public void setDocumentsToLoad(List<Resource> documentsToLoad) {
+        this.documentsToLoad = documentsToLoad;
+    }
+
+    private List<Resource> documentsToLoad;
+
 
     public String getVectorStorePath() {
         return this.vectorStorePath;
@@ -17,4 +27,9 @@ public class VectorStoreProperties {
     public void setVectorStorePath(String vectorStorePath1) {
         this.vectorStorePath=vectorStorePath1;
     }
+
+    public List<Resource> getDocumentsToLoad() {
+        return documentsToLoad;
+    }
+
 }
