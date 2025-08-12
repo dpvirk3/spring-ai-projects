ffb9ec2a972bb20a9a476287ba3533e67c3d6347
dpvirk3 <dpvirk3@gmail.com>
2025-08-12 15:04:37 +0530

    Using documents as source for RAG.

--- spring-ai-rag-ollama/spring-ai-rag-ollama/src/main/java/com/dv/spring_ai_rag_ollama/services/OllamaServiceImpl.java
+++ spring-ai-rag-ollama/spring-ai-rag-ollama/src/main/java/com/dv/spring_ai_rag_ollama/services/OllamaServiceImpl.java
@@ -6,12 +6,17 @@ import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class OllamaServiceImpl implements OllamaService {
@@ -30,9 +35,15 @@ public class OllamaServiceImpl implements OllamaService {
     @Override
     public Answer getAnswer(Question question) {
 
+        List<Document> documents = vectorStore.similaritySearch(SearchRequest.builder()
+                        .query(question.question()).topK(5).build());
+        List<String> contentList = documents.stream().map(Document::getText).toList();
 
-        PromptTemplate promptTemplate = new PromptTemplate(question.question());
-        Prompt prompt = promptTemplate.create();
+        PromptTemplate promptTemplate = new PromptTemplate(ragPromptTemplate);
+        Prompt prompt = promptTemplate.create(Map.of("input", question.question(),
+                "documents", String.join("\n", contentList)));
+
+        contentList.forEach(System.out::println);
 
         ChatResponse response = chatModel.call(prompt);
 
